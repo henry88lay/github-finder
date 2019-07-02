9193713f83bdf2dc39849db862bd1db35ea34628
Henry Lay <henry88lay@gmail.com>
2019-07-02 23:16:22 +1000

    built out the component for userItems

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Navbar from './component/layout/Navbar';
+import UserItem from './component/users/UserItem';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className='App'>
         <Navbar />
+        <UserItem />
       </div>
     );
   }
