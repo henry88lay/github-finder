39e26ac43620ec4772fdcc492e6b2b1c046bccb8
Henry Lay <henry88lay@gmail.com>
2019-07-07 20:01:37 +1000

    refactoring the home component and adding page not found component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './component/layout/Navbar';
-import Users from './component/users/Users';
+import Home from './component/pages/Home';
 import User from './component/users/User';
-import Search from './component/users/Search';
 import Alert from './component/layout/Alert';
 import About from './component/pages/About';
+import NotFound from './component/pages/NotFound';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
 import './App.css';
@@ -20,18 +20,10 @@ const App = () => {
             <div className='container'>
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={props => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
